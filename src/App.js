5f9310347d52b5d05ecd47be3d9d6e170d2ce0e8
Suwetha <suwedhashna23@gmail.com>
2023-02-26 11:48:47 +0530

    created the price cart in ReactJS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Card from './card';
 
 function App() {
+
+  let details=[{
+    grade:"FREE",
+    price:0,
+    period:"month",
+    user:"Single User",
+    storage:"5GB Storage",
+    project:"Unlimited Public Projects",
+    access:"Community Access",
+    privateproject:"Unlimited Private Projects",
+    support:"Dedicated Phone Support",
+    subdomain:"Free Subdomain",
+    reports:"Monthly Status Reports"
+  },
+  {
+    grade:"PLUS",
+    price:9,
+    user:"5 User",
+    storage:"50GB Storage",
+    project:"Unlimited Public Projects",
+    access:"Community Access",
+    privateproject:"Unlimited Private Projects",
+    support:"Dedicated Phone Support",
+    subdomain:"Free Subdomain",
+    reports:"Monthly Status Reports"
+  },
+{
+    grade:"PRO",
+    price:49,
+    user:"Unlimited User",
+    storage:"150GB Storage",
+    project:"Unlimited Public Projects",
+    access:"Community Access",
+    privateproject:"Unlimited Private Projects",
+    support:"Dedicated Phone Support",
+    subdomain:"Unlimited Free Subdomain",
+    reports:"Monthly Status Reports"
+  }]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <section class="pricing py-5">
+  <div class="container">
+    <div class="row">
+      {
+       details.map((detail)=>{
+          return <Card data={detail}></Card>
+       })
+      }
+      
     </div>
+  </div>
+  </section>
   );
 }
 
